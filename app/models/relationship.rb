2e725f9806f40c046c59c1876e2a77c6f6a7e84d
bhoff704 <bhoff704@gmail.com>
2015-12-16 19:47:21 -0500

    bug fix

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,6 +3,6 @@ class Relationship < ActiveRecord::Base
     belongs_to :user
     belongs_to :friend, class_name: "user"
     validates :user_id, presence: true
-    validates: friend_id, presence: true
+    validates :friend_id, presence: true
 
 end
